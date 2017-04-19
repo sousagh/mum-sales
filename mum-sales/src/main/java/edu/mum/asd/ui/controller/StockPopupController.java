e1258e959631e3575e21f016f4c15bbcd8d28820
Gustavo Sousa <gustavosousa@Gustavos-MacBook-Pro.local>
2017-04-19 11:02:32 -0500

    Added interaction messages on the stock popup

--- mum-sales/src/main/java/edu/mum/asd/ui/controller/StockPopupController.java
+++ mum-sales/src/main/java/edu/mum/asd/ui/controller/StockPopupController.java
@@ -4,9 +4,13 @@ import edu.mum.asd.domain.Product;
 import edu.mum.asd.framework.ApplicationContext;
 import edu.mum.asd.framework.di.AutoInjected;
 import edu.mum.asd.framework.di.BaseController;
+import edu.mum.asd.framework.validation.ValidationResult;
+import edu.mum.asd.framework.validation.annotation.NotNullValidation;
 import edu.mum.asd.service.ProductService;
 import edu.mum.asd.ui.util.ApplicationConstants;
 import edu.mum.asd.ui.util.SearchTableEntry;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -30,7 +34,14 @@ public class StockPopupController extends BaseController{
     private Text stockText;
 
     @FXML
+    @NotNullValidation(message = "New stock cannot be null.")
     private TextField updateText;
+
+    @FXML
+    private Text successMessage;
+
+    @FXML
+    private Text errorMessage;
 
     private SearchTableEntry rowData;
 
@@ -46,13 +57,38 @@ public class StockPopupController extends BaseController{
         this.descriptionText.setText(rowData.getItemDescription());
         this.priceText.setText(String.valueOf(rowData.getProduct().getPrice()));
         this.stockText.setText(String.valueOf(rowData.getProduct().getQuantity()));
+
+        this.updateText.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    StockPopupController.this.updateText.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
     }
 
     @FXML
     public void updateStock(){
-        System.out.println("update!" + updateText.getText());
-        Product product = rowData.getProduct();
-        product.setQuantity(new Integer(updateText.getText()));
-        productService.updateProduct(product);
+        ValidationResult validate = this.validate();
+        if(validate.valid()){
+            try {
+                Product product = rowData.getProduct();
+                product.setQuantity(new Integer(updateText.getText()));
+                productService.updateProduct(product);
+                successMessage.setVisible(true);
+                errorMessage.setVisible(false);
+
+            } catch (Exception e) {
+                successMessage.setVisible(false);
+                errorMessage.setText("Error!");
+                errorMessage.setVisible(true);
+            }
+        } else {
+            errorMessage.setVisible(true);
+            successMessage.setVisible(false);
+            errorMessage.setText(validate.getErrors().get(0).getMessage());
+        }
+
     }
 }
